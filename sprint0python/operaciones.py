54b0fbf11f784b357e7e5a1572757e5a5ac316fc
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-09-27 10:29:03 +0200

    Ejercicio 3 finalizado

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,12 @@
+print("Introduce el primer número")
+n1= int (input());
+print("Introduce el segundo número")
+n2= int (input());
+
+print("Suma",(n1+n2))
+print("Resta",(n1-n2))
+print("Multiplicación",(n1*n2))
+if(n1!=0):
+    print("Division",(n1/n2))
+else:
+    print("No se permite divisiones entre 0")
