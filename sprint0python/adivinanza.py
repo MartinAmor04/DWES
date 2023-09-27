d07a3c33d45832894074452e94a2a47b228de63e
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-09-27 10:18:33 +0200

    Ejercicio 2 finalizado

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,8 +1,33 @@
 
+puntos=0
 print("¿Qué parece oro y plata no es? Escoge:[a(Plátano)/b(Sandía)/c(Manzana)] ")
 respuesta=input();
 
 if respuesta=="a":
     print("Acertaste!!!")
+    puntos+=10
 else:
     print("Fallaste :(")
+    puntos-=5
+
+print("¿Que animal empieza a cuatro patas, luego dos y por ultimo seis? Escoge:[a(Elefante)/b(Humano)/c(Araña)] ")
+respuesta=input();
+
+if respuesta=="b":
+    print("Acertaste!!!")
+    puntos+=10
+else:
+    print("Fallaste :(")
+    puntos-=5
+
+print("¿Qué es aquello que al nombrarlo desaparece? Escoge:[a(Miedo)/b(Viento)/c(Silencio)] ")
+respuesta=input();
+
+if respuesta=="c":
+    print("Acertaste!!!")
+    puntos+=10
+else:
+    print("Fallaste :(")
+    puntos-=5
+
+print("Total de puntos: ",puntos)
