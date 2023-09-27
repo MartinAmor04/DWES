7d517440cb230113e9d3ef6ae6bbf7f16ef3adb1
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-09-27 10:12:55 +0200

    Ejercicio 1 finalizado

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,8 @@
+
+print("¿Qué parece oro y plata no es? Escoge:[a(Plátano)/b(Sandía)/c(Manzana)] ")
+respuesta=input();
+
+if respuesta=="a":
+    print("Acertaste!!!")
+else:
+    print("Fallaste :(")
