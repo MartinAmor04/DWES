b7b802db112a8b3505f2e136effbb819aaf61936
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-10-01 17:45:23 +0200

    Ejercicio 6

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,33 +1,28 @@
+import random
 
 puntos=0
-print("¿Qué parece oro y plata no es? Escoge:[a(Plátano)/b(Sandía)/c(Manzana)] ")
-respuesta=input();
 
+lista=["¿Qué parece oro y plata no es? Escoge:[a(Plátano)/b(Sandía)/c(Manzana)] ",
+    "¿Que animal empieza a cuatro patas, luego dos y por ultimo seis? Escoge:[a(Humano)/b(Elefante)/c(Araña)] ",
+    "¿Qué es aquello que al nombrarlo desaparece? Escoge:[a(Silencio)/b(Viento)/c(Miedo)] "]
+    
+adv1, adv2 = (random.sample(lista, 2)) 
+print(adv1)
+respuesta=input()
 if respuesta=="a":
-    print("Acertaste!!!")
-    puntos+=10
+     print("Correcto!")
+     puntos=int (puntos+10)
 else:
-    print("Fallaste :(")
-    puntos-=5
+        print("Fallaste!")
+        puntos=int (puntos-5)
 
-print("¿Que animal empieza a cuatro patas, luego dos y por ultimo seis? Escoge:[a(Elefante)/b(Humano)/c(Araña)] ")
-respuesta=input();
-
-if respuesta=="b":
-    print("Acertaste!!!")
-    puntos+=10
+print(adv2)
+respuesta=input()
+if respuesta=="a":
+        print("Correcto!")
+        puntos=int (puntos+10)
 else:
-    print("Fallaste :(")
-    puntos-=5
-
-print("¿Qué es aquello que al nombrarlo desaparece? Escoge:[a(Miedo)/b(Viento)/c(Silencio)] ")
-respuesta=input();
-
-if respuesta=="c":
-    print("Acertaste!!!")
-    puntos+=10
-else:
-    print("Fallaste :(")
-    puntos-=5
+        print("Fallaste!")
+        puntos=int (puntos-5)
 
 print("Total de puntos: ",puntos)
