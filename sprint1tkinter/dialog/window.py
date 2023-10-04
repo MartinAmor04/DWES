b0dda4b1debb9ae5e856afa2ac235ee556129d9a
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-10-04 10:10:48 +0200

    Ejercicio 2 finalizado

---
+++ sprint1tkinter/dialog/window.py
@@ -0,0 +1,18 @@
+from tkinter import ttk, Button
+
+class MainWindow:
+    def on_button_click(self):
+        pass
+    def __init__(self, root):
+        self.root=root
+
+        self.frame=ttk.Frame(self.root)
+        self.frame.pack()
+
+        self.label=ttk.Label(self.frame, text="Este mensaje es poco importante")
+        self.label.pack()
+
+        self.button=ttk.Button(self.frame, text="OK", command= self.on_button_click)
+        self.button.pack()
+
+        
