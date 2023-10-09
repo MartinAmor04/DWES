66fe56aa02d2ca2683b5f1f44fc3adbe064dad54
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-10-09 11:50:18 +0200

    Ejercicio 6 finalizado

--- sprint1tkinter/catalog/mainWindow.py
+++ sprint1tkinter/catalog/mainWindow.py
@@ -2,6 +2,7 @@ from tkinter import  ttk
 import tkinter as tk
 from cell import Cell
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
 class MainWindow():
     
@@ -10,22 +11,26 @@ class MainWindow():
         messagebox.showinfo("Información"+ message) 
 
     def __init__(self, root):
-        root.title("MainWindow")
-        
+        root.title("Peliculas")
+
         self.cells=[
-            Cell("Imagen 1", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\edited\\apocalypse-now-(edited).jpg"),
-            Cell("Imagen 2", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\edited\\evangelion(edited).jpg"),
-            Cell("Imagen 3", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\edited\\le haine(edited).jpg"),
-            Cell("Imagen 4 ", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\edited\\the lighthouse(edited).jpg"),
-            Cell("Imagen 5", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\edited\\the lobster(edited).jpg")
-
-
+            Cell("Imagen 1", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\apocalypse-now-.jpg"),
+            Cell("Imagen 2", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\evangelion.jpg"),
+            Cell("Imagen 3", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\le haine.jpg"),
+            Cell("Imagen 4 ", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\the lighthouse.jpg"),
+            Cell("Imagen 5", "C:\\msys64\\home\\marti\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\the lobster.jpg")
         ]
 
-        for i,cell in enumerate(self.cells):
-            label = ttk.Label(root, image=cell.image_tk, text=cell.title, compound=tk.BOTTOM)
-            label.grid(row=i, column=0)
-            label.bind("<Button-1>",lambda event, cell = cell: self.onButtonClicked(cell))
+        for i, cell in enumerate(self.cells):
+            foto = Image.open(cell.path)
+            img = foto.resize((100, 100), Image.Resampling.LANCZOS)
+
+            cell.image_tk = ImageTk.PhotoImage(img)
+
+            label= ttk.Label(root, image=cell.image_tk, text=cell.title, compound=tk.BOTTOM)
+            label.grid(row=0, column=i)
+            label.bind("<Button-1>", lambda event, celda = cell: self.onButtonClicked(cell))
+
 
    
         
