c17b7f0355542e423619304a29ec194070230943
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-10-11 09:59:24 +0200

    Tarea 7 finalizada

--- sprint1tkinter/catalog/cell.py
+++ sprint1tkinter/catalog/cell.py
@@ -2,7 +2,13 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class Cell:
-    def __init__(self, title, path):
+    #Clase la cual contiene la estructuras de las celdas
+    def __init__(self, title, path,description):
      self.title=title
      self.path=path
-     self.image_tk= ImageTk.PhotoImage(file =self.path)
+     self.foto = Image.open(self.path)
+     self.img = self.foto.resize((100, 100), Image.Resampling.LANCZOS)
+     self.image_tk = ImageTk.PhotoImage(self.img)     
+     self.description=description
+
+     
