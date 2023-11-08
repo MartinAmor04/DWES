3086aae714cc5d90b739aafaf50f360acc9460c8
MartinAmor04 <145535289+MartinAmor04@users.noreply.github.com>
2023-11-08 11:43:41 +0100

    Comentado DetailActivity.java

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -5,7 +5,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//visualiza la ventana detail_activity.xml
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
