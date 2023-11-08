43924549020627bdbd8aa7d0af93e6d7316ad1fb
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2023-11-08 09:55:06 +0100

    Tarea 5 finalizada

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -9,8 +9,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
-private Button boton;
-private Activity activity=this;
+    private Button boton;
+    private Activity activity=this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
